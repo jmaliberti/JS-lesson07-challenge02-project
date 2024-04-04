c97edd98253d2529f3fc407f8362778fc735c47c
Jessica Aliberti <jmaliberti@gmail.com>
2024-04-04 12:44:56 -0700

    Created all variables for drop-down list selections and answers.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // Your JS code here
-var subTypeElement = document.querySelector ("#subscription");
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+
